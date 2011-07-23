0e3944b426c10a3d7e79c4a25f8eef37463d2b37
Jirka Helmich <jiri@helmich.cz>
2011-07-23 18:30:30 +0200

    initial

---
+++ AventioCMS/Models/DomainModel.Designer.cs
@@ -0,0 +1,2 @@
+﻿// Seeing this comment implies that the code generation for file 'c:\users\jirka\documents\visual studio 2010\Projects\AventioCMS\AventioCMS\Models\DomainModel.edmx'
+// failed. See the ErrorList for details.
